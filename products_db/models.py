4df9ca93697a35904c55fe5567c4ebcef4a436ce
Georgio <georgioji@gmail.com>
2025-01-08 00:36:26 -0700

    Added entries to app models

--- products_db/models.py
+++ products_db/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
 
 class Stores(models.Model):
@@ -29,6 +31,7 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     brand = models.CharField(max_length=256)
     product_name = models.CharField(max_length=256)
+    quantity = models.IntegerField()
     weight = models.DecimalField(max_digits=5, decimal_places=2)
     weight_unit = models.CharField(max_length=256, choices=(('kg', 'kg'), ('lb', 'lb'), ('oz', 'oz'), ('l', 'l'), ('ml', 'ml')))
 
@@ -38,9 +41,18 @@ class Products(models.Model):
 
     last_update = models.DateTimeField(auto_now=True)
     last_updated_by = models.CharField(max_length=256)
+    last_edited_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.brand + self.product_name
+        return self.brand + ' - ' + self.product_name
 
     class Meta:
         ordering = ['sku']
+
+
+class ProductPriceHistory(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.DO_NOTHING)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    save_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.product.name} ({self.save_date})"
